f9c21e9e0a97219e9ff25cccf1c4ae6ce9eb946e
redru <luzenobi@gmail.com>
2016-02-11 00:37:44 +0100

    Aggiunta funzionalita' di inserimento nel conto

--- app/js/controller/conto-controller.js
+++ app/js/controller/conto-controller.js
@@ -1,6 +1,23 @@
 (function() {
     app.controller('conto-controller', ['$scope', '$rootScope', '$http',
         function ($scope, $rootScope, $http) {
+            $http.get('/bank/getEntries')
+                .then(function (response) {
+                    $scope.entries = JSON.parse(response.data);
+                });
 
+            // Definizione delle funzioni dello $scope
+            $scope.addEntry = function() {
+                $http({
+                    method: 'POST',
+                    url: '/bank/addEntry',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: JSON.stringify($scope.addEntryModel)
+                }).then(function (response) {
+                    response.data == 'OK' ? alert('Inserimento avvenuto correttamente.') : alert('Problemi durante l\'inserimento.');
+                });
+            }
         }]);
 })();
--- bank_routes.js
+++ bank_routes.js
@@ -24,11 +24,41 @@ router.get('/getUsers', function(req, res) {
     bankDB.all('SELECT * FROM USERS', function(err, rows) {
         if (err) {
             console.log(err);
-            res.status(404).send('Internal error.');
+            res.status(500).send('Internal error.');
         }
 
         res.status(200).type('json').json(JSON.stringify(rows));
     });
+
+    bankDB.close();
+});
+
+router.get('/getEntries', function(req, res) {
+    var bankDB = new sqlite3.Database('./data/BANK.db');
+    bankDB.all('SELECT * FROM ACCOUNT_ENTRY', function(err, rows) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal error.');
+        }
+
+        res.status(200).type('json').json(JSON.stringify(rows));
+    });
+
+    bankDB.close();
+});
+
+router.post('/addEntry', function(req, res) {
+    var bankDB = new sqlite3.Database('./data/BANK.db');
+    var entry = req.body.entry;
+
+    bankDB.run('INSERT INTO ACCOUNT_ENTRY (USER_ID, ACCOUNT_ID, VALUE, CAUSAL, DATE) VALUES($USER_ID, $ACCOUNT_ID, $VALUE, $CAUSAL, $DATE)',
+        { $USER_ID: entry.USER_ID, $ACCOUNT_ID: entry.ACCOUNT_ID, $VALUE: entry.VALUE, $CAUSAL: entry.CAUSAL, $DATE: entry.DATE},
+        function(err) {
+            err ? res.status(500).send('Internal error.') : res.status(200).send('OK');
+
+        });
+
+    bankDB.close();
 });
 
 module.exports = router;
