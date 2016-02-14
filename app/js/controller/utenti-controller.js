876003d258c41d2827e93cdf81cc077d0bb5ac58
redru <luzenobi@gmail.com>
2016-02-14 09:26:10 +0100

    Aggiornate funzionalita' Conto

--- app/js/controller/utenti-controller.js
+++ app/js/controller/utenti-controller.js
@@ -1,9 +1,31 @@
 (function() {
-    app.controller('utenti-controller', ['$scope', '$rootScope', '$http',
-        function ($scope, $rootScope, $http) {
-            $http.get('/bank/getUsers')
-                .then(function (response) {
-                    $scope.users = JSON.parse(response.data);
-                });
-        }]);
+
+    var UtentiController = function($scope, $rootScope, $http) {
+        var _this = this;
+
+        $scope.getUsers = function() {
+            _this.getUsers($scope, $http);
+        }
+
+        /*$scope.doStuff = function() {
+            _this.doStuff();
+        };*/
+
+        _this.getUsers($scope, $http);
+    };
+
+    UtentiController.prototype.getUsers = function($scope, $http) {
+        $http.get('/bank/getUsers')
+            .then(function (response) {
+                $scope.users = JSON.parse(response.data);
+            });
+    };
+
+    UtentiController.prototype.doStuff = function() {
+
+    };
+
+    UtentiController.$inject = [ '$scope', '$rootScope', '$http' ];
+    app.controller('utenti-controller', UtentiController);
+
 })();
